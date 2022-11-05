f5eb3d2b2a1de45ceaaa4baee3b9c111e9789681
Super IT <itmasterservice@hotmail.com>
2022-11-06 01:55:04 +0900

    fixed stripe api

--- server/api/cart.post.js
+++ server/api/cart.post.js
@@ -2,7 +2,8 @@ import { config } from "process";
 import Stripe from "stripe";
 export default defineEventHandler(async (event) => {
     const body = await useBody(event);    
-    const stripeSecret = useRuntimeConfig().stripeSecret;
+    // const stripeSecret = useRuntimeConfig().stripeSecret;
+    const stripeSecret = 'sk_test_51LIthZJWhNxJ2tl450T8qolmFohol5vTQCQrBjYOkVtM6z0eCCa2E3OmaPTXPsS6FA5Fz39j3CeWBFP3yGSywLA100FrYZXwWa';
     const stripe = new Stripe(stripeSecret);
 
     const res = await stripe.products.list({
@@ -14,9 +15,16 @@ export default defineEventHandler(async (event) => {
         quantity: body.products.find((item) => item.id === product.id).quantity,
     }))
 
+    // const session = await stripe.checkout.sessions.create({
+    //     cancel_url: "http://localhost:3000/cart",
+    //     success_url: "http://localhost:3000/checkout/success",
+    //     mode: "payment",
+    //     line_items: lineItems
+    // })
+
     const session = await stripe.checkout.sessions.create({
-        cancel_url: "http://localhost:3000/cart",
-        success_url: "http://localhost:3000/checkout/success",
+        cancel_url: "https://sauce-shopping.stormkit.dev/cart",
+        success_url: "https://sauce-shopping.stormkit.dev/success",
         mode: "payment",
         line_items: lineItems
     })
