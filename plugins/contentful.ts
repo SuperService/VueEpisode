8437f8127fb94307bc17ff6a7d7aa25c451a36da
Super IT <itmasterservice@hotmail.com>
2022-11-06 01:42:33 +0900

    fixed

--- plugins/contentful.ts
+++ plugins/contentful.ts
@@ -4,7 +4,8 @@ export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig();
   const client = contentful.createClient({
     space: config.public.contentfulSpace,
-    accessToken: config.public.contentfulPublicAccessToken,
+    // accessToken: config.public.contentfulPublicAccessToken,
+    accessToken: '1uuS9K4kK-0PEVF8hiPdW44_5rda7LxZgJVr6dYJZqQ',
   });
   return {
     provide: {
